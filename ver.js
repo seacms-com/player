13b79046015fa1230ff079f778592bf04c5af159
seacms-com <90815686+seacms-com@users.noreply.github.com>
2022-11-13 10:40:50 +0800

    2020/11/13

---
+++ ver.js
@@ -0,0 +1,78 @@
+console.log(btjson_ver);
+if(btjson_ver == "1.0.8"){
+	document.write("<script language=javascript src='https://player-cdn.dyttq.com/js/bofangqi/js/md5.js'></script>");
+	document.write("<script language=javascript src='https://player-cdn.dyttq.com/js/bofangqi/js/jquery.min.js'></script>");
+	document.write("<script language=javascript src='https://player-cdn.dyttq.com/js/bofangqi/js/btjson1.0.8.js'></script>");
+	document.write("<script language=javascript src='https://player-cdn.dyttq.com/js/bofangqi/js/btjsonplayer1.0.8.js'></script>");
+	document.write("<script language=javascript src='https://player-cdn.dyttq.com/js/bofangqi/js/layer.min.js'></script>");
+}
+if(btjson_ver == "1.0.7"){
+	document.write("<script language=javascript src='https://player-cdn.dyttq.com/js/bofangqi/js/md5.js'></script>");
+	document.write("<script language=javascript src='https://player-cdn.dyttq.com/js/bofangqi/js/jquery.min.js'></script>");
+	document.write("<script language=javascript src='https://player-cdn.dyttq.com/js/bofangqi/js/btjson1.0.7.js'></script>");
+	document.write("<script language=javascript src='https://player-cdn.dyttq.com/js/bofangqi/js/btjsonplayer1.0.7.js'></script>");
+	document.write("<script language=javascript src='https://player-cdn.dyttq.com/js/bofangqi/js/layer.min.js'></script>");
+}
+if(btjson_ver == "1.0.6"){
+	document.write("<script language=javascript src='https://player-cdn.dyttq.com/js/bofangqi/js/md5.js'></script>");
+	document.write("<script language=javascript src='https://player-cdn.dyttq.com/js/bofangqi/js/jquery.min.js'></script>");
+	document.write("<script language=javascript src='https://player-cdn.dyttq.com/js/bofangqi/js/btjson1.0.6.js'></script>");
+	document.write("<script language=javascript src='https://player-cdn.dyttq.com/js/bofangqi/js/btjsonplayer1.0.6.js'></script>");
+	document.write("<script language=javascript src='https://player-cdn.dyttq.com/js/bofangqi/js/layer.min.js'></script>");
+}
+if(btjson_ver == "1.0.5"){
+	document.write("<script language=javascript src='https://player-cdn.dyttq.com/js/bofangqi/js/md5.js'></script>");
+	document.write("<script language=javascript src='https://player-cdn.dyttq.com/js/bofangqi/js/jquery.min.js'></script>");
+	document.write("<script language=javascript src='https://player-cdn.dyttq.com/js/bofangqi/js/btjson1.0.5.js'></script>");
+	document.write("<script language=javascript src='https://player-cdn.dyttq.com/js/bofangqi/js/btjsonplayer1.0.5.js'></script>");
+	document.write("<script language=javascript src='https://player-cdn.dyttq.com/js/bofangqi/js/layer.min.js'></script>");
+}
+if(btjson_ver == "1.0.4"){
+	document.write("<script language=javascript src='https://player-cdn.dyttq.com/js/bofangqi/js/md5.js'></script>");
+	document.write("<script language=javascript src='https://player-cdn.dyttq.com/js/bofangqi/js/jquery.min.js'></script>");
+	document.write("<script language=javascript src='https://player-cdn.dyttq.com/js/bofangqi/js/btjson1.0.4.js'></script>");
+	document.write("<script language=javascript src='https://player-cdn.dyttq.com/js/bofangqi/js/btjsonplayer1.0.4.js'></script>");
+	document.write("<script language=javascript src='https://player-cdn.dyttq.com/js/bofangqi/js/layer.min.js'></script>");
+}
+if(btjson_ver == "1.0.3"){
+	document.write("<script language=javascript src='https://player-cdn.dyttq.com/js/bofangqi/js/md5.js'></script>");
+	document.write("<script language=javascript src='https://player-cdn.dyttq.com/js/bofangqi/js/jquery.min.js'></script>");
+	document.write("<script language=javascript src='https://player-cdn.dyttq.com/js/bofangqi/js/btjson1.0.3.js'></script>");
+	document.write("<script language=javascript src='https://player-cdn.dyttq.com/js/bofangqi/js/btjsonplayer1.0.3.js'></script>");
+	document.write("<script language=javascript src='https://player-cdn.dyttq.com/js/bofangqi/js/layer.min.js'></script>");
+}
+
+if(btjson_ver == "1.0.2"){
+	document.write("<script language=javascript src='https://player-cdn.dyttq.com/js/bofangqi/js/md5.js'></script>");
+	document.write("<script language=javascript src='https://player-cdn.dyttq.com/js/bofangqi/js/jquery.min.js'></script>");
+	document.write("<script language=javascript src='https://player-cdn.dyttq.com/js/bofangqi/js/btjson1.0.2.js'></script>");
+	document.write("<script language=javascript src='https://player-cdn.dyttq.com/js/bofangqi/js/btjsonplayer1.0.2.js'></script>");
+	document.write("<script language=javascript src='https://player-cdn.dyttq.com/js/bofangqi/js/layer.min.js'></script>");
+}
+
+if(btjson_ver == "1.0.1"){
+	document.write("<script language=javascript src='https://player-cdn.dyttq.com/js/bofangqi/js/md5.js'></script>");
+	document.write("<script language=javascript src='https://player-cdn.dyttq.com/js/bofangqi/js/jquery.min.js'></script>");
+	document.write("<script language=javascript src='https://player-cdn.dyttq.com/js/bofangqi/js/btjson1.0.2.js'></script>");
+	document.write("<script language=javascript src='https://player-cdn.dyttq.com/js/bofangqi/js/btjsonplayer1.0.2.js'></script>");
+	document.write("<script language=javascript src='https://player-cdn.dyttq.com/js/bofangqi/js/layer.min.js'></script>");
+}
+
+
+if(btjson_ver == "1.0.0"){
+	document.write("<script language=javascript src='https://player-cdn.dyttq.com/js/bofangqi/js/md5.js'></script>");
+	document.write("<script language=javascript src='https://player-cdn.dyttq.com/js/bofangqi/js/jquery.min.js'></script>");
+	document.write("<script language=javascript src='https://cdn.jsdelivr.net/gh/btjson/5mrk@0.0.2.1/js/btjson.min.js'></script>");
+	document.write("<script language=javascript src='https://player-cdn.dyttq.com/js/bofangqi/js/btjsonplayer1.0.0.js'></script>");
+	document.write("<script language=javascript src='https://player-cdn.dyttq.com/js/bofangqi/js/layer.min.js'></script>");
+}
+
+
+if(btjson_ver == "0.3.8"){
+
+	document.write("<script language=javascript src='https://player-cdn.dyttq.com/js/bofangqi/js/md5.js'></script>");
+	document.write("<script language=javascript src='https://player-cdn.dyttq.com/js/bofangqi/js/jquery.min.js'></script>");
+	document.write("<script language=javascript src='https://cdn.jsdelivr.net/gh/btjson/5mrk@0.0.3/js/btjson.min.js'></script>");
+	document.write("<script language=javascript src='https://player-cdn.dyttq.com/js/bofangqi/js/btjsonplayer0.3.8.js'></script>");
+	document.write("<script language=javascript src='https://player-cdn.dyttq.com/js/bofangqi/js/layer.min.js'></script>");
+}
